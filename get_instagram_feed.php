ffc01e13930995574ac63bb4f9b8407db9f0d52b
Boylett <boylett@users.noreply.github.com>
2018-06-04 22:01:13 +0100

    Update get_instagram_feed.php

--- get_instagram_feed.php
+++ get_instagram_feed.php
@@ -9,24 +9,24 @@
 	function get_instagram_feed($username)
 	{
 		$username = trim($username);
-        $endpoint = 'https://www.instagram.com/' . $username;
+        	$endpoint = 'https://www.instagram.com/' . $username;
 		$feed     = array();
 
 		$page = @file_get_contents($endpoint);
 
-        if($page)
-        {
-            preg_match("/window\._sharedData\s?=\s?(.*?);?\s?<\/script>/i", $page, $data);
+		if($page)
+		{
+		    preg_match("/window\._sharedData\s?=\s?(.*?);?\s?<\/script>/i", $page, $data);
 
-            if(isset($data[1]) and preg_match("/^({|\[)/", $data[1]) and preg_match("/(}|\])$/", $data[1]))
-            {
-                $data = @json_decode($data[1], true);
+		    if(isset($data[1]) and preg_match("/^({|\[)/", $data[1]) and preg_match("/(}|\])$/", $data[1]))
+		    {
+			$data = @json_decode($data[1], true);
 
-                if($data)
-                {
-                    return $data;
-                }
-            }
+			if($data)
+			{
+			    return $data;
+			}
+		    }
 		}
 
 		return $feed;
